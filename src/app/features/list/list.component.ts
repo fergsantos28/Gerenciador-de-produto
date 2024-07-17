19128acbac8e5fbc874fc117b2c89c6acfd53e49
fergsantos28 <fernandogarcia102031@gmail.com>
2024-07-17 20:58:01 -0300

    refactor: cria ConfirmationDialogService  para especificar a confirmacao

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,64 +1,33 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
 import { MatButtonModule } from '@angular/material/button';
 import { CardComponent } from './components/card/card.component';
-import { Router, RouterLink } from '@angular/router';
-import {MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { filter } from 'rxjs';
-
-@Component({
-  selector: 'app-confirmation-dialog',
-  template: `
-      <h2 mat-dialog-title>Deletar produto</h2>
-    <mat-dialog-content>
-      Realmente deseja deletar este item?
-    </mat-dialog-content>
-    <mat-dialog-actions align="center">
-      <button mat-button (click)="onNo()">No</button>
-      <button mat-raised-button color="accent" (click)="onYes()" cdkFocusInitial>Yes</button>
-    </mat-dialog-actions>
-    `,
-
-  standalone: true,
-  imports: [MatButtonModule, MatDialogModule,],
-})
-export class ConfirmationDialogComponentDialog {
-
-  matDialogRef = inject(MatDialogRef);
-
-    onNo() {
-      this.matDialogRef.close(false);
-
-    }
-  onYes(){
-    this.matDialogRef.close(true);
-
-  }
-  }
+import { ConfirmationDialogService } from '../../shared/services/confirmation-dialog.service';
 
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent,RouterLink,MatButtonModule],
+  imports: [CardComponent, RouterLink, MatButtonModule,],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
+  styleUrl: './list.component.scss',
 })
 export class ListComponent {
-  
-  products: Product[] = [];  
-  productsService = inject(ProductsService);  
-  router = inject(Router);
-  matDialog = inject(MatDialog);
-  confirmationDialogService: any;
-  
+  products = signal<Product[]>(
+    inject(ActivatedRoute).snapshot.data['products']
+  );
 
-  
-  
+  productsService = inject(ProductsService);
+  router = inject(Router);
+  confirmationDialogService = inject(ConfirmationDialogService);
+
+    
   ngOnInit(){
     this.productsService.getAll().subscribe((products) => {
-      this.products = products;
+      this.products.set(products);
     });
   }
   
@@ -66,16 +35,14 @@ export class ListComponent {
     this.router.navigate(['/edit-product', product.id]);
   }
   
-  onDelete(product: Product) {
-    this.matDialog
-      .open(ConfirmationDialogComponentDialog)
-      .afterClosed()
+ onDelete(product: Product) {
+    this.confirmationDialogService
+      .openDialog()
       .pipe(filter((answer) => answer === true))
-      
       .subscribe(() => {
         this.productsService.delete(product.id).subscribe(() => {
           this.productsService.getAll().subscribe((products) => {
-            this.products = products;
+            this.products.set(products);
           });
         });
       });
